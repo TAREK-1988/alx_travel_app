eec818ea99b1cb4a5342dc34c171ec9672fe5a8e
TAREK <tareksayedmokhtar@yahoo.com>
2025-10-31 21:54:49 +0200

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,8 +1,6 @@
-# alx_travel_app/listings/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['GET'])
 def health(request):
     return Response({"status": "ok"})
-
